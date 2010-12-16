5aa58005f756600d149c83bcf7cd3b1009933294
Alex Sexton <AlexSexton@gmail.com>
2010-12-15 23:31:01 -0600

    adding sleeping capability for more obvious out of order loading testing

--- tests/css/css.php
+++ tests/css/css.php
@@ -1,5 +1,10 @@
 <?php
 
 header('Content-Type: text/css');
-$num = basename($_SERVER['REQUEST_URI'], '.css');
+$qs = '';
+if (isset($_GET['sleep'])) {
+  sleep($_GET['sleep']);
+  $qs = '?sleep=' . $_GET['sleep'];
+}
+$num = basename($_SERVER['REQUEST_URI'], '.css' . $qs);
 echo '#item_' . $num . ' { z-index:' . $num  . '; }';
--- tests/js/javascript.php
+++ tests/js/javascript.php
@@ -1,4 +1,8 @@
 <?php
-
-	header('Content-Type: text/javascript');
-	echo 'window.' . basename($_SERVER['REQUEST_URI'], '.js') . ' = true;';
+header('Content-Type: text/javascript');
+$qs = '';
+if (isset($_GET['sleep'])) {
+  sleep($_GET['sleep']);
+  $qs = '?sleep=' . $_GET['sleep'];
+}
+echo 'window.' . basename($_SERVER['REQUEST_URI'], '.js' . $qs) . ' = true;';
