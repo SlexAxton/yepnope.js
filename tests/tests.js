8a4e997dc4263b22ae633a844eea7c6daad6ac02
Alex Sexton <AlexSexton@gmail.com>
2010-12-30 03:57:16 -0600

    Adding lots of Input tests to make sure we always accept the same types of inputs in the future

--- tests/tests.js
+++ tests/tests.js
@@ -40,7 +40,7 @@ if ( ! window.console ) {
     }, 0);
   }
 
-  var timeout   = 15000,
+  var timeout   = 20000,
   		rgb				= (function( i ){  			
   			var a = [];
   			while ( i-- ) a.push( Math.floor( Math.random() * 255 ) );
@@ -48,8 +48,90 @@ if ( ! window.console ) {
   		})(3),
       u         = (+new Date);
 
+  module("Input Support")
+  asyncTest("Accept as many input types as possible", 8, function() {
+    ++u;
+
+    // single string
+    yepnope("js/a"+u+".js");
+    // since we'd like to test just single string input, give the test for the existence of this one a long 
+    // wait time and then a check. This isn't foolproof (could show false-positives), but worthwhile for making
+    // sure that a common practice is well-supported. Increase the timeout if you have slow internet connection, etc.
+    setTimeout(function(){
+      ok( w['a'+u], "Single string*");
+    }, 3000); // 3 seconds of leeway
+
+    // array of strings
+    yepnope(['js/b'+u+'.js', 'js/c'+u+'.js']);
+    setTimeout(function(){
+      ok( w['b'+u] && w['c'+u], "Array of strings*");
+    }, 3000);
+
+    // single object
+    yepnope({
+      load: 'js/d'+u+'.js',
+      callback: function() {
+        ok(w['d'+u], "Single Object (with `load` keyword)");
+      }
+    });
+
+    // single object (with both keyword instead)
+    yepnope({
+      both: 'js/e'+u+'.js',
+      callback: function() {
+        ok(w['e'+u], "Single Object (with `both` keyword)");
+      }
+    });
+
+    // single object with array of strings inside
+    yepnope({
+      load: ['js/f'+u+'.js', 'js/g'+u+'.js'],
+      complete: function() {
+        ok(w['f'+u] && w['g'+u], "Single object with array of strings inside");
+      }
+    });
+
+    // array of objects
+    yepnope([{
+      load: 'js/h'+u+'.js?sleep=1',
+      wait: true // force an in order execution manually (since we're not testing the wait! prefix yet)
+    },
+    {
+      load: 'js/i'+u+'.js',
+      callback: function() {
+        ok(w['h'+u] && w['i'+u], "Array of objects (forces order)");
+      }
+    }]);
+
+    // mixed array of strings and objects
+    yepnope(['js/j'+u+'.js', {
+      load: 'js/k'+u+'.js?sleep=3', // use the sleep like the crappy timeout hack to test the string
+      callback: function() {
+        ok(w['j'+u] && w['k'+u], "Mixed array of strings and objects*");
+      }
+    }]);
+
+    // array of objects with array of strings inside
+    yepnope([{
+      load: ['js/l'+u+'.js', 'js/m'+u+'.js'],
+      complete: function() {
+        ok(w['l'+u] && w['m'+u], "Array of objects with array of strings inside");
+      }
+    }]);
+    
+    // We do not intentionally support any deeper nesting than this of arrays and objects, but won't actively prevent it.
+    
+    // Since we're using crappy logic to test the single string loads, we have to start the tests crappily as well
+    setTimeout(function(){
+      start();
+    }, 6000);
+  });
+
   module("Asynchronous Script Loading")
   asyncTest("Non-recursive loading of a &rarr; b &rarr; c", 9, function() {
+    // Increment the unique value per test, so caching doesn't occur between tests
+    ++u;
+
     yepnope([
       {
         load : 'js/a'+u+'.js?sleep=2',
@@ -83,6 +165,8 @@ if ( ! window.console ) {
   });
 
   asyncTest("Recursive loading of d &rarr; e &rarr; f &rarr; g &rarr; h", 25, function() {
+    ++u;
+
     yepnope([
       {
         load : 'js/d'+u+'.js',
@@ -185,18 +269,20 @@ if ( ! window.console ) {
     stop(timeout);
   });
   asyncTest("Key Value Callbacks", 2, function() {
+    ++u;
+
     yepnope([
       {
         load : {
-          'myscript-aa': 'js/aa'+u+'.js',
-          'myscript-bb': 'js/bb'+u+'.js'
+          'myscript-a': 'js/a'+u+'.js',
+          'myscript-b': 'js/b'+u+'.js'
         },
         callback : {
-          'myscript-aa': function() {
-            ok( w['aa'+u], "aa has loaded");
+          'myscript-a': function() {
+            ok( w['a'+u], "a has loaded");
           },
-          'myscript-bb': function() {
-            ok( w['bb'+u], "bb has loaded");
+          'myscript-b': function() {
+            ok( w['b'+u], "b has loaded");
           }
         },
         complete: function() {
@@ -208,6 +294,7 @@ if ( ! window.console ) {
   });
 
   asyncTest("404 Fallback", 2, function() {
+    ++u;
     yepnope([
       {
         load : 'iDoesNotExist',
@@ -226,6 +313,53 @@ if ( ! window.console ) {
               start();
             }
           })
+        }
+      }
+    ]);
+    stop(timeout);
+  });
+
+  module("Supported Plugins")
+  asyncTest("autoprotocol supported global filter plugin", 1, function() {
+    ++u;
+    yepnope([
+      {
+        load : '//' + w.location.hostname + (w.location.port ? ':'+w.location.port : '') + (w.location.pathname.replace('index.html', '')) + 'js/a'+u+'.js',
+        callback : function(url, res, key, yepnope){
+          ok( w['a'+u], "The correct script was loaded with the // prefix");
+        },
+        complete: function() {
+          start();
+        }
+      }
+    ]);
+    stop(timeout);
+  });
+
+  asyncTest("IE prefix test", 2, function() {
+    ++u;
+    yepnope([
+      {
+        load: 'ie!js/a'+u+'.js',
+        callback : function(url, res, key, yepnope){
+          // The script uses IE conditionals, so we'll cross check with user-agent... could be bad, idk, good enough for now.
+          if ($.browser.msie) {
+            ok( w['a'+u], "The browser is IE, and the script was loaded");
+          }
+        }
+      },
+      {
+        // we need to load another script that _would_ wait if the ie one loaded, and wont wait if it doesn't.
+        load: 'js/b'+u+'.js',
+        callback: function() {
+          // Might as well make sure this script loaded, even though that's already in another test
+          ok(w['b'+u], "Other scripts still load after an ie prefixed script.");
+          if (!$.browser.msie) {
+            ok(!w['a'+u], "The browser is not IE, but the complete callback was called an no script was loaded.");
+          }
+        },
+        complete: function() {
+          start();
         }
       }
     ]);
