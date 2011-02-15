cb7c3f77742c01df9ed1d36a10fa461dad503830
Ralph Holzmann <ralphholzmann@gmail.com>
2011-02-15 14:22:01 -0600

    Added no-cache option to javascript.php.

--- tests/js/javascript.php
+++ tests/js/javascript.php
@@ -1,7 +1,15 @@
 <?php
 header('Content-Type: text/javascript');
-header('Cache-Control: max-age=60, must-revalidate');
-header("Expires: Thu, 31 Dec 2020 20:00:00 GMT");
+if ( strrpos( $_SERVER['REQUEST_URI'], 'no-cache' ) === FALSE ) {
+	header('Cache-Control: max-age=60, must-revalidate');
+	header("Expires: Thu, 31 Dec 2020 20:00:00 GMT");
+} else {
+	$pretty_modtime = gmdate('D, d M Y H:i:s') . 'GMT'; 
+	header('Cache-Control: max-age=0, must-revalidate');
+	header("Last-Modified: $pretty_modtime"); 
+	header("Expires: $pretty_modtime"); 
+	header("Pragma: no-cache"); 
+}
 $subject = $_SERVER['REQUEST_URI'];
 $pattern = '/\/sleep-(\d+)\//';
 preg_match($pattern, $subject, $matches);
