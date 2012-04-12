7b019d9148715c206c00dfc02bc70dcdc0d35d19
Kenneth Powers <mail@kenpowers.net>
2012-04-12 16:50:48 -0400

    Removed dependency on `Array.indexOf(...)`.

--- filters/yepnope.pathfilter.js
+++ filters/yepnope.pathfilter.js
@@ -10,10 +10,6 @@
  *	   load: ['google/jquery/1.7.2/jquery.min.js', 'google/jqueryui/1.8.18/jquery-ui.min.js', 'my-cdn/style.css', '/non/path/directory/file.js']
  *   });
  *
- * Requirements:
- *   The browser must implement Array.indexOf(...). If you have to support browsers which don't implement this method please make sure you define it.
- *   See: http://soledadpenades.com/2007/05/17/arrayindexof-in-internet-explorer/
- *
  * Official Yepnope Plugin
  *
  * WTFPL License
@@ -26,12 +22,10 @@
       yn.addFilter(function (resource) {
         // check each url for path
         for (path in yn.paths) {
-          if (resource.url.indexOf(path) === 0) {
-            resource.url = resource.url.replace(path, yn.paths[path]);
-            return resource;
-          }
+          resource.url = resource.url.replace(new RegExp('^' + path), yn.paths[path]);
+          return resource;
         }
-        //carry on my wayward, son
+        // carry on my wayward, son
         return resource;
       });
     };
